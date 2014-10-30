da8cd057b804d82ed29a0fe2dd79b83e141179a6
globalavocado <globalavocado@users.noreply.github.com>
2014-10-30 17:34:41 +0000

    temperature can decrease, all tests passed

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -22,4 +22,18 @@ describe("Thermostat", function() {
         expect(thermostat.temperature).toEqual (25);
     })
   })
+
+  describe('decreasing the temperature', function(){
+    it ('decreases if > 10 degrees', function(){
+      thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual (19);
+     })
+
+    it ('does not decrease if the temperature is <= 10 degrees', function(){
+      thermostat.temperature = 10;
+
+      thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual (10);
+    })
+  })
 });
